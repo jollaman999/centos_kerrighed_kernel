bae67cac49d7a32d94874cf8310c20c9755c4cef
Bjorn Helgaas <bjorn.helgaas at hp.com>
2009-06-05 14:37:23 +0000

    pnp: add PNP resource range checking function
    Add a PNP resource range check function, indicating whether a resource
    has been assigned to any device.
    
    Signed-off-by: Bjorn Helgaas <bjorn.helgaas@hp.com>
    [apw@canonical.com: fixed up exports et al]
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Eric Anholt <eric@anholt.net>

--- include/linux/pnp.h
+++ include/linux/pnp.h
@@ -459,6 +459,7 @@ int pnp_start_dev(struct pnp_dev *dev);
 int pnp_stop_dev(struct pnp_dev *dev);
 int pnp_activate_dev(struct pnp_dev *dev);
 int pnp_disable_dev(struct pnp_dev *dev);
+int pnp_range_reserved(resource_size_t start, resource_size_t end);
 
 /* protocol helpers */
 int pnp_is_active(struct pnp_dev *dev);
@@ -489,6 +490,7 @@ static inline int pnp_start_dev(struct pnp_dev *dev) { return -ENODEV; }
 static inline int pnp_stop_dev(struct pnp_dev *dev) { return -ENODEV; }
 static inline int pnp_activate_dev(struct pnp_dev *dev) { return -ENODEV; }
 static inline int pnp_disable_dev(struct pnp_dev *dev) { return -ENODEV; }
+static inline int pnp_range_reserved(resource_size_t start, resource_size_t end) { return 0;}
 
 /* protocol helpers */
 static inline int pnp_is_active(struct pnp_dev *dev) { return 0; }
