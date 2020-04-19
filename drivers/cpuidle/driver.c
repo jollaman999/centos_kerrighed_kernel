53534ec82fb0056d9fce757340ccd298db770bc7
ish <ish@innogrid.com>
2020-04-19 12:59:14 +0900

    cpuidle: Add intel_idle driver

--- drivers/cpuidle/driver.c
+++ drivers/cpuidle/driver.c
@@ -40,6 +40,15 @@ int cpuidle_register_driver(struct cpuidle_driver *drv)
 EXPORT_SYMBOL_GPL(cpuidle_register_driver);
 
 /**
+ * cpuidle_get_driver - return the current driver
+ */
+struct cpuidle_driver *cpuidle_get_driver(void)
+{
+	return cpuidle_curr_driver;
+}
+EXPORT_SYMBOL_GPL(cpuidle_get_driver);
+
+/**
  * cpuidle_unregister_driver - unregisters a driver
  * @drv: the driver
  */
