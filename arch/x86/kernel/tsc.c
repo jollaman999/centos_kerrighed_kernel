fe8217c64a19011f49ba597c3b31c7ef92bb1a10
ish <ish@innogrid.com>
2020-04-22 18:02:57 +0900

    x86: tsc: Fix ‘dummy’ may be used uninitialized warning message

--- arch/x86/kernel/tsc.c
+++ arch/x86/kernel/tsc.c
@@ -631,7 +631,7 @@ static int time_cpufreq_notifier(struct notifier_block *nb, unsigned long val,
 				void *data)
 {
 	struct cpufreq_freqs *freq = data;
-	unsigned long *lpj, dummy;
+	unsigned long *lpj, dummy = 0;
 
 	if (cpu_has(&cpu_data(freq->cpu), X86_FEATURE_CONSTANT_TSC))
 		return 0;
