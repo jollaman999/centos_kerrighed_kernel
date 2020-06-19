6f8982e943c2fb6abf2f1420ec2c7145b4810ca7
ish <ish@innogrid.com>
2020-06-19 10:23:20 +0900

    epm: children: Fix the comment in krg_wait_consider_task()

--- kerrighed/epm/children.c
+++ kerrighed/epm/children.c
@@ -749,6 +749,7 @@ bool krg_wait_consider_task(struct remote_child *child,
 /*                  +| */
 /*                 if (wo->notask_error) */
 /*                         wo->notask_error = ret; */
+/*                 return false; */
 /*         } */
 
 	if (unlikely(child->ptraced)) {
